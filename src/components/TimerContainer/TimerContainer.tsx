2bb086f1b23a3256864e0cb746c6868874ed577d
haru <nguyen.johnly25>
2025-04-21 08:09:43 -0700

    testing part 2

--- src/components/TimerContainer/TimerContainer.tsx
+++ src/components/TimerContainer/TimerContainer.tsx
@@ -6,7 +6,7 @@ import { updateMode, updateTimeLeft } from "../../slices/timerSlice";
 import { useDispatch } from "react-redux";
 import { modeTypes } from "../../slices/timerSlice";
 
-//testing 
+//testing adlkfjaf
 export default function TimerContainer() {
     const timer = useTimer()
     const dispatch = useDispatch()
